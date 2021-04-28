feafa60d0db83e0183742d10fcc54f618a9f5486
Samyush <samyushmaharjan@gmail.com>
2021-04-28 13:31:18 +0545

    books controller adding author

--- AppDev2ndCW/Controllers/BooksController.cs
+++ AppDev2ndCW/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppDev2ndCW.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,8 +9,17 @@ namespace AppDev2ndCW.Controllers
 {
     public class BooksController : Controller
     {
+        private readonly DataBaseContext dataBaseContext;
+
+        public BooksController(DataBaseContext db)
+        {
+            dataBaseContext = db;
+        }
+            
         public IActionResult Index()
         {
+            //pass data
+
             return View();
         } 
 
@@ -33,18 +43,55 @@ namespace AppDev2ndCW.Controllers
             return View();
         }
 
-        public IActionResult Category()
+        [HttpPost]
+        public async Task<IActionResult> AddBook(BookInventory books, string bookName, int quantity, int rate, int author, int category)
+        {   
+            books.Book_name = bookName;
+            books.Stock_Quantity = quantity;
+            books.Price = rate;
+            books.Author_Id = author;
+            books.Category_Id = category;
+            books.Stocked_Date = DateTime.Now;
+            try
+            {
+                dataBaseContext.BookInventory.Add(books);
+                await dataBaseContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+       
+        public IActionResult AddAuthor(BookAuthor authorIs)
         {
-                
+            //authorIs.Id = 1;
+            authorIs.Author = "ram";
+            dataBaseContext.BookAuthors.Add(authorIs);
+            dataBaseContext.SaveChanges();
             /*BookInventory bki */
             return View();
         }
 
-        public IActionResult AddAuthor()
-        {
 
-            /*BookInventory bki */  
-            return View();
+        [HttpPost]
+        public async Task<IActionResult> AddAuthor(BookAuthor authorIs, string authorName, int id)
+        {
+            authorIs.Id = id;
+            authorIs.Author = authorName;
+            
+            try
+            {
+                dataBaseContext.BookAuthors.Add(authorIs);
+                await dataBaseContext.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            catch (Exception)
+            {
+                return null;
+            }
         }
     }
 }
