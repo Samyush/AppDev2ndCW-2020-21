d43bfcf68049f97f01e472e830ca5bb2f5ef74a7
Urusha Shrestha <urusha10@gmail.com>
2021-04-29 15:56:21 +0545

    sale data rendered

--- AppDev2ndCW/Controllers/SalesController.cs
+++ AppDev2ndCW/Controllers/SalesController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AppDev2ndCW.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,16 @@ namespace AppDev2ndCW.Controllers
 {
     public class SalesController : Controller
     {
+        private readonly DataBaseContext databaseContext;
+
+        public SalesController(DataBaseContext db)
+        {
+            databaseContext = db;
+        }
         public IActionResult List()
         {
-            return View();
+            var saleList = databaseContext.Sales.ToArray();
+            return View(saleList);
         }
 
         public IActionResult SaleItems()
@@ -22,11 +30,43 @@ namespace AppDev2ndCW.Controllers
         {
             return View();
         }
+        public async Task<IActionResult> AddSale(Sales sale, DateTime sale_date, int customer_id, int sale_total)
+        {
+            sale.Sale_Date = sale_date;
+            sale.Customer_Id = customer_id;
+            sale.Sale_Total = sale_total;
+            try
+            {
+                databaseContext.Sales.Add(sale);
+                await databaseContext.SaveChangesAsync();
+                return Redirect("Home");
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
         public IActionResult AddSaleItem()
         {
             return View();
         }
-
+        public async Task<IActionResult> AddSaleItem(SaleItems saleItems, int sale_id, int book_id, int quantity, int total)
+        {
+            saleItems.Sale_Id = sale_id;
+            saleItems.Book_Id = book_id;
+            saleItems.Quantity = quantity;
+            saleItems.Total = total;
+            try
+            {
+                databaseContext.SaleItems.Add(saleItems);
+                await databaseContext.SaveChangesAsync();
+                return Redirect("Home");
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
         public IActionResult GenerateBill()
         {
             return View();
